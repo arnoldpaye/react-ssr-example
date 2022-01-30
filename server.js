e4a8ae7b9611bc99a1fabff3a11b7317a719a80f
Arnold Paye <arnoldpayet@gmail.com>
2022-01-30 12:14:16 -0400

    add InitialDataContext

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import { StaticRouter } from 'react-router-dom/server';
 import path from 'path';
 import fs from 'fs';
 import App from './src/App';
+import { InitialDataContext } from './src/InitialDataContext';
 
 global.window = {};
 
@@ -24,15 +25,31 @@ app.get('/api/articles', (req, res) => {
   res.json(loadedArticles);
 });
 
-app.get('/*', (req, res) => {
+app.get('/*', async (req, res) => {
   const sheet = new ServerStyleSheet();
 
-  const reactApp = renderToString(
+  const contextObj = { _isServerSide: true, _requests: [], _data: {} };
+
+  renderToString(
     sheet.collectStyles(
-      <StaticRouter location={req.url}>
-      <App />
-    </StaticRouter>
+      <InitialDataContext.Provider value={contextObj}>
+        <StaticRouter location={req.url}>
+          <App />
+        </StaticRouter>
+      </InitialDataContext.Provider>
     )
+  );
+
+  await Promise.all(contextObj._requests);
+  contextObj._isServerSide = false;
+  delete contextObj._requests;
+
+  const reactApp = renderToString(
+    <InitialDataContext.Provider value={contextObj}>
+      <StaticRouter location={req.url}>
+        <App />
+      </StaticRouter>
+    </InitialDataContext.Provider>
   );
 
   const templateFile = path.resolve('./build/index.html');
@@ -41,10 +58,8 @@ app.get('/*', (req, res) => {
       return res.status(500).send(err);
     }
 
-    const loadedArticles = articles;
-
     return res.send(
-      data.replace('<div id="root"></div>', `<script>window.preloadedArticles = ${JSON.stringify(loadedArticles)};</script><div id="root">${reactApp}</div>`)
+      data.replace('<div id="root"></div>', `<script>window.preloadedData = ${JSON.stringify(contextObj)};</script><div id="root">${reactApp}</div>`)
           .replace('{{ styles }}', sheet.getStyleTags())
     )
   });
